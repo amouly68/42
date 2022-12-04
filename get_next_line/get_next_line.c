3952913252772ce00a25f225ea2b19dabd0fa0f7
Antoine Mouly <amouly@k0r4p7.42mulhouse.fr>
2022-12-04 13:35:56 +0100

    fin de travail dimanche

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,24 +6,39 @@
 /*   By: amouly <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 14:43:47 by amouly            #+#    #+#             */
-/*   Updated: 2022/12/01 15:35:15 by amouly           ###   ########.fr       */
+/*   Updated: 2022/12/04 13:35:39 by amouly           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	get_next_char(int fd)
+char	*move_curser(char *str, ret)
 {
-	char buf[1];
+	int	a;
+	char * ret;
 
-	if(read(fd, buf, 1) != 1)
-		return (0);
-	return (*buf);
+	a = 0;
+	while (str[a] != '\n' && str[a] != '\0')
+
+
+
 }
-
 
 
 char	*get_next_line(int fd)
 {
-	static char *buffer
+	static char *stock;
+	char	*val_return;
+
+	if (stock != NULL)
+	{
+		
+	
+	}		
+	if (read (fd, stock, BUFFER_SIZE) > 0)
+	{
+		
+	
+	}
+
 }
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,22 +6,19 @@
 /*   By: amouly <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 15:34:04 by amouly            #+#    #+#             */
-/*   Updated: 2022/12/04 11:40:27 by amouly           ###   ########.fr       */
+/*   Updated: 2022/12/04 13:35:42 by amouly           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE = 5 
+#  define BUFFER_SIZE 10
+
 
 #  include <stdlib.h>
 #  include <unistd.h>
 
-typedef struct linked_list {
-	char content;
-	struct linked_list *next;
-};
 
 char	*get_next_line(int fd);
 
