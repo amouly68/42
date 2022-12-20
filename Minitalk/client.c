9e3e13e1ff67e623638381b88f89a48e2e986b14
Antoine Mouly <amouly@k0r4p7.42mulhouse.fr>
2022-12-20 09:18:56 +0100

    minitalk fini

--- Minitalk/client.c
+++ Minitalk/client.c
@@ -6,7 +6,7 @@
 /*   By: amouly <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/18 13:28:53 by amouly            #+#    #+#             */
-/*   Updated: 2022/12/19 18:13:53 by amouly           ###   ########.fr       */
+/*   Updated: 2022/12/20 09:16:44 by amouly           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,74 +15,36 @@
 #include <signal.h>
 #include <sys/types.h>
 
-/*void char_to_byte(const unsigned char c, pid_t pid)
-{
-	unsigned char car;
-	unsigned char modulo;
-	int	i;
-	
-	car = c;
-	i = 1;
-	while (car > 0)
-	{
-		modulo = car % 2;
-		if (modulo == 0)
-		{
-			kill(pid, SIGUSR1);
-			ft_printf("0");
-		}
-		else if (modulo == 1)
-		{
-			kill(pid, SIGUSR2);
-			ft_printf("1");
-		}
-		car = car / 2;
-		i++;
-		usleep(150);
-	}
-	while (i < 9)
-	{
-		kill(pid, SIGUSR1);
-		ft_printf("0\n --NOUVELLE LETTRE -- \n");
-		i++;
-		usleep(150);
-	}
-}*/
-
 void	char_to_byte(unsigned char c, pid_t pid)
 {
-	int	i = 0;
-	int j = 128;
+	int	i;
+	int	j;
+
+	if (c > 128 || pid < 0)
+		return ;
+	i = 0;
+	j = 128;
 	while (i < 8)
 	{
 		if ((c / j) % 2 == 0)
-		{
-			ft_printf("0");
 			kill(pid, SIGUSR1);
-		}
-		else 
-		{
-			ft_printf("1");
+		else
 			kill(pid, SIGUSR2);
-		}
 		j = j / 2;
 		i++;
 		usleep(150);
 	}
 }
 
-
-
-
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-	int pid;
+	int	pid;
 	int	i;	
-	
-	pid = ft_atoi(argv[1]);
-	i = 0;
+
 	if (argc != 3)
 		ft_printf("Nombre d'arguments invalide\n");
+	pid = ft_atoi(argv[1]);
+	i = 0;
 	while (argv[2][i])
 	{
 		char_to_byte(argv[2][i], pid);
--- Minitalk/server.c
+++ Minitalk/server.c
@@ -6,7 +6,7 @@
 /*   By: amouly <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/18 13:28:35 by amouly            #+#    #+#             */
-/*   Updated: 2022/12/19 18:26:10 by amouly           ###   ########.fr       */
+/*   Updated: 2022/12/20 09:17:47 by amouly           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,50 +15,10 @@
 #include "libft/libft.h"
 #include <signal.h>
 
-/*void	handler(int signal)
-{
-	static unsigned char	car = 0;
-	
-	if ((int)car > 128)
-	{
-		car = car << 1;
-		ft_putchar_fd(car, 1);
-		car = 0;
-	}	
-	while ((int)car <= 128)
-	{
-		if (signal == SIGUSR1)
-			car = car >> 1;
-		else if (signal == SIGUSR2)
-		{
-			car = car | 128;	
-			car = car >> 1;
-		}
-	}
-	if car
-}
-*/
-
 void	handler(int signal)
 {
-	static	unsigned char car = 255;
-	/*static	int i = 0;
+	static unsigned char	car = 255;
 
-	if (signal == SIGUSR2)
-	{
-		car = (car << 1) + 1;
-	}
-	else
-	{
-		car <<= 1;
-	}
-	if (i == 7)
-	{
-		write(1, &car, 1);
-		car = 0;
-		i = -1;
-	}
-	i++;*/
 	if (signal == SIGUSR1)
 		car = car << 1;
 	else if (signal == SIGUSR2)
@@ -74,29 +34,16 @@ void	handler(int signal)
 	usleep(150);
 }
 
-
-
-
-int main()
+int	main(void)
 {
-	pid_t 	pid;
+	pid_t	pid;
 
 	pid = getpid();
-	//int i = 0;
 	ft_printf("Le pid est : %d\n", pid);
 	signal(SIGUSR1, handler);
 	signal(SIGUSR2, handler);
 	while (1)
 	{
-	//		pause();
-		/*while (i < 8)
-		{
-			signal(SIGUSR1, handler);
-			signal(SIGUSR2, handler);
-			pause();
-			i++;
-		}
-		i = 0;
-		ft_printf("\n --- Nouvelle Lettre --- \n");*/
+		pause();
 	}
 }
