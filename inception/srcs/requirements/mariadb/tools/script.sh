50ee4d36c0ea91bfaecff3837d9b9201ce00cba4
Antoine Mouly <amouly@k2r3p4.42mulhouse.fr>
2023-11-29 12:28:21 +0100

    mariadb ok

--- inception/srcs/requirements/mariadb/tools/script.sh
+++ inception/srcs/requirements/mariadb/tools/script.sh
@@ -1,23 +1,42 @@
 #!/bin/bash
+set -e
 
-service mariadb start 
+# Attend que MariaDB soit prête
+function wait_for_mariadb() {
+    while ! mysqladmin ping -h localhost --silent --user=root --password=""; do
+        sleep 1
+        echo "En attente de la base de données MariaDB..."
+    done
+}
 
-# Variables pour la base de données et l'utilisateur
-db_name="wordpress"
-db_user="toto"
-db_pwd="totototo"
+# Initialisation de la base de données
+function initialize_db() {
+    # Définit des valeurs par défaut si les variables d'environnement ne sont pas définies
+    MYSQL_DATABASE=${MYSQL_DATABASE:-default_db}
+    MYSQL_USER=${MYSQL_USER:-user}
+    MYSQL_PASSWORD=${MYSQL_PASSWORD:-password}
+    MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD:-rootpassword}
 
+    # Commandes SQL pour initialiser la base de données et les utilisateurs
+    mysql --user=root --password="" -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
+    mysql --user=root --password="" -e "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
+    mysql --user=root --password="" -e "GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';"
+    mysql --user=root --password="" -e "FLUSH PRIVILEGES;"
+    mysql --user=root --password="" -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
+}
 
+# Démarre le serveur MariaDB en arrière-plan
+mysqld_safe &
 
-echo "CREATE DATABASE IF NOT EXISTS $db_name ;" > db1.sql
-echo "CREATE USER IF NOT EXISTS '$db_user'@'%' IDENTIFIED BY '$db_pwd' ;" >> db1.sql
-echo "GRANT ALL PRIVILEGES ON $db_name.* TO '$db_user'@'%' ;" >> db1.sql
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '12345' ;" >> db1.sql
-echo "FLUSH PRIVILEGES;" >> db1.sql
+# Attente de la disponibilité de MariaDB
+wait_for_mariadb
 
-mysql < db1.sql
+# Exécution des opérations d'initialisation
+initialize_db
 
-kill $(cat /var/run/mysqld/mysqld.pid)
+# Arrête le serveur MariaDB pour le redémarrer proprement
+mysqladmin --user=root --password="$MYSQL_ROOT_PASSWORD" shutdown
 
-mysqld
-
+# Lance le serveur MariaDB comme processus principal (PID 1)
+exec mysqld
+doc
