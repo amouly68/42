4c88f117075a346bc136e44ae917a576f9138c8b
Antoine Mouly <amouly@k0r3p4.42mulhouse.fr>
2023-11-30 17:16:28 +0100

    fin jeudi

--- inception/srcs/requirements/wordpress/tools/script.sh
+++ inception/srcs/requirements/wordpress/tools/script.sh
@@ -10,7 +10,7 @@ done
 # Utiliser wp-cli pour configurer WordPress et créer les utilisateurs
 cd /var/www/html
 wp core install --url="$DOMAIN_NAME" --title="$WP_TITLE" --admin_user="$WP_ADMIN_USER" --admin_password="$WP_ADMIN_PWD" --admin_email="$WP_ADMIN_EMAIL" --allow-root
-wp user create "$WP_USER1" "$WP_USER1_EMAIL" --role=author --user_pass="$WP_USER1_PWD" --skip-email --allow-root
+wp user create "$WP_USER1" "$WP_USER1_EMAIL" --role=author --user_pass="$WP_USER1_PWD" --allow-root
 
 # Exécuter php-fpm en tant que processus principal
 exec php-fpm
