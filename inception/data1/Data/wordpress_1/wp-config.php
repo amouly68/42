4d0939936d89b026793aa8ca69ffd95e61cc37a9
Antoine Mouly <amouly@k0r3p7.42mulhouse.fr>
2023-12-01 16:17:10 +0100

    fin vendredi

---
+++ inception/data1/Data/wordpress_1/wp-config.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * The base configuration for WordPress
+ *
+ * The wp-config.php creation script uses this file during the
+ * installation. You don't have to use the web site, you can
+ * copy this file to "wp-config.php" and fill in the values.
+ *
+ * This file contains the following configurations:
+ *
+ * * MySQL settings
+ * * Secret keys
+ * * Database table prefix
+ * * ABSPATH
+ *
+ * @link https://codex.wordpress.org/Editing_wp-config.php
+ *
+ * @package WordPress
+ */
+
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define( 'DB_NAME', 'wordpress');
+
+/** MySQL database username */
+define( 'DB_USER', 'user_data');
+
+/** MySQL database password */
+define( 'DB_PASSWORD', 'data_password');
+
+/** MySQL hostname */
+define( 'DB_HOST', 'mariadb:3306');
+
+/** Database Charset to use in creating database tables. */
+define( 'DB_CHARSET', 'utf8');
+
+/** The Database Collate type. Don't change this if in doubt. */
+define( 'DB_COLLATE', '');
+
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define( 'AUTH_KEY',         '359cf270d74fd2972342bd4cd469eb9a48d70746');
+define( 'SECURE_AUTH_KEY',  '455ba8deaf3fb60f639450e9d54f3892248d89f0');
+define( 'LOGGED_IN_KEY',    '7778a40cb80728189cd1373cdab6d18153b9c56c');
+define( 'NONCE_KEY',        '19b5aed8d339b2311e3850031655e5171658f1df');
+define( 'AUTH_SALT',        'ca49637492c4d3413a5f3a553cd904168be15260');
+define( 'SECURE_AUTH_SALT', 'a7a9336a7f9a6c53cf1e573f7371f4cc265fd93c');
+define( 'LOGGED_IN_SALT',   'd10dfe6bab8476c5e5feffb6e86da841b25a4bf4');
+define( 'NONCE_SALT',       'a7cadf19149d8232bba64847844bc1281b551a69');
+
+/**#@-*/
+
+/**
+ * WordPress Database Table prefix.
+ *
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix = 'wp_';
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the Codex.
+ *
+ * @link https://codex.wordpress.org/Debugging_in_WordPress
+ */
+define( 'WP_DEBUG', false );
+
+// If we're behind a proxy server and using HTTPS, we need to alert Wordpress of that fact
+// see also http://codex.wordpress.org/Administration_Over_SSL#Using_a_Reverse_Proxy
+if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
+	$_SERVER['HTTPS'] = 'on';
+}
+
+/* That's all, stop editing! Happy publishing. */
+
+/** Absolute path to the WordPress directory. */
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', dirname( __FILE__ ) . '/' );
+}
+
+/** Sets up WordPress vars and included files. */
+require_once( ABSPATH . 'wp-settings.php' );
--- inception/srcs/requirements/wordpress/tools/script.sh
+++ inception/srcs/requirements/wordpress/tools/script.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 set -e
 
-# Attendre que la base de données soit prête
-until nc -z mariadb 3306; do
-  >&2 echo "MariaDB is unavailable - sleeping"
-  sleep 1
-done
-
-# Utiliser wp-cli pour configurer WordPress et créer les utilisateurs
-cd /var/www/html
-wp core install --url="$DOMAIN_NAME" --title="$WP_TITLE" --admin_user="$WP_ADMIN_USER" --admin_password="$WP_ADMIN_PWD" --admin_email="$WP_ADMIN_EMAIL" --allow-root
-wp user create "$WP_USER1" "$WP_USER1_EMAIL" --role=author --user_pass="$WP_USER1_PWD" --allow-root
-
 # Créer le répertoire pour le socket, si nécessaire
 mkdir -p /run/php
 
@@ -21,11 +10,35 @@ chown www-data:www-data /run/php
 # Chemin du fichier de configuration PHP-FPM
 WWW_CONF="/etc/php/7.4/fpm/pool.d/www.conf"
 
-# Configurer PHP-FPM pour utiliser un socket Unix
-sed -i 's/^listen = .*/listen = \/run\/php\/php7.4-fpm.sock/' $WWW_CONF
+# # Configurer PHP-FPM pour utiliser un socket Unix
+# sed -i 's/^listen = .*/listen = \/run\/php\/php7.4-fpm.sock/' $WWW_CONF
 
+# Configurer PHP-FPM pour utiliser le port TCP 9000
+sed -i 's/^listen = .*/listen = 9000/' $WWW_CONF
+
+# Utiliser wp-cli pour configurer WordPress et créer les utilisateurs
+cd /var/www/html
+
+if ! wp core is-installed --allow-root; then
+    wp core install --url="$DOMAIN_NAME" --title="$WP_TITLE" --admin_user="$WP_ADMIN_USER" --admin_password="$WP_ADMIN_PWD" --admin_email="$WP_ADMIN_EMAIL" --allow-root
+fi
+
+# Vérifier si l'utilisateur existe déjà
+if ! wp user get "$WP_USER1" --allow-root; then
+    wp user create "$WP_USER1" "$WP_USER1_EMAIL" --role=author --user_pass="$WP_USER1_PWD" --allow-root
+fi
+
+if ! wp theme is-installed twentysixteen --allow-root; then
+    wp theme install twentysixteen --activate --allow-root
+fi
+
+
+
+# wp core install --url="$DOMAIN_NAME" --title="$WP_TITLE" --admin_user="$WP_ADMIN_USER" --admin_password="$WP_ADMIN_PWD" --admin_email="$WP_ADMIN_EMAIL" --allow-root
+# wp user create "$WP_USER1" "$WP_USER1_EMAIL" --role=author --user_pass="$WP_USER1_PWD" --allow-root
+# wp theme install twentysixteen --activate
 
 # Exécuter php-fpm en tant que processus principal
-exec /usr/sbin/php-fpm7.4
+exec /usr/sbin/php-fpm7.4 -F
 
 
