6428ad545957975ac2918d6f42afb5fb5b6300e4
Antoine <antoine.mouly@gmail.com>
2023-12-01 08:32:57 +0100

    debut vendredi

--- inception/srcs/requirements/wordpress/tools/script.sh
+++ inception/srcs/requirements/wordpress/tools/script.sh
@@ -12,6 +12,20 @@ cd /var/www/html
 wp core install --url="$DOMAIN_NAME" --title="$WP_TITLE" --admin_user="$WP_ADMIN_USER" --admin_password="$WP_ADMIN_PWD" --admin_email="$WP_ADMIN_EMAIL" --allow-root
 wp user create "$WP_USER1" "$WP_USER1_EMAIL" --role=author --user_pass="$WP_USER1_PWD" --allow-root
 
+# Créer le répertoire pour le socket, si nécessaire
+mkdir -p /run/php
+
+# Assurer que www-data (ou autre utilisateur PHP-FPM) peut écrire dans ce répertoire
+chown www-data:www-data /run/php
+
+# Chemin du fichier de configuration PHP-FPM
+WWW_CONF="/etc/php/7.4/fpm/pool.d/www.conf"
+
+# Configurer PHP-FPM pour utiliser un socket Unix
+sed -i 's/^listen = .*/listen = \/run\/php\/php7.4-fpm.sock/' $WWW_CONF
+
+
 # Exécuter php-fpm en tant que processus principal
-exec php-fpm
+exec /usr/sbin/php-fpm7.4
+
 
